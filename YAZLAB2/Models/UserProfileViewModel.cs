e5896c57ea3f7c44a4e73d6cc17c3e0839cefccc
Çağla <114026286+caglagok@users.noreply.github.com>
2024-11-26 22:54:12 +0300

    bazı şeyler eklendi
    .

---
+++ YAZLAB2/Models/UserProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace YAZLAB2.Models
+{
+    public class UserProfileViewModel
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Email { get; set; }
+        public string TelefonNumarasi { get; set; }
+        public DateTime? DogumTarihi { get; set; }
+        public string ProfilFoto { get; set; }
+        public string Konum { get; set; }
+        public List<string> IlgiAlanlari { get; set; }
+        public string Cinsiyet { get; set; }
+        public string UserName { get; set; }
+    }
+}
